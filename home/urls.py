d2d31f24e4db66998e1c355b63df3ef812398345
Aryasah <75942764+Aryasah@users.noreply.github.com>
2021-03-17 04:34:24 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,15 +1,19 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
 
+from django.urls import path
+from django.contrib import admin
+from . import views
+from home import views
+from django.conf.urls import url,include
 urlpatterns = [
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
     path("contact", views.contact, name='contact'), 
     path('login',views.loginUser, name="login"),
     path('logout',views.logoutUser, name="logout"),
-    path('register',views.register, name="register"),
-    path('test',views.test, name="test")
+    
+    url(r'^signup/$', views.signup, name='signup'),
+
+    
 
 
 ]
